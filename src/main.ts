c2021df713e9efdb9ee08f21959d6dc5f6871d6c
Crmal <pjm2207@naver.com>
2022-11-12 23:41:03 +0900

    feat: GlobalInterceptors로 response DTO data필드 생성

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new WrapResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
